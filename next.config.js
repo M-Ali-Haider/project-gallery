237ac71be89a77b3af5dd180985be4912f44adaf
M-Ali-Haider <mah01134212100@gmail.com>
2024-02-08 04:48:59 +0500

    i think i found a way

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    basePath:'/projectGallery',
+    basePath:'/project-gallery',
 };
 export default nextConfig;
